e27b66eb5d0d51f4817dfa7ffe220fd31aa80c93
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2024-01-15 21:13:35 +0900

    判定わかりやすく

--- contPanel.cpp
+++ contPanel.cpp
@@ -24,8 +24,7 @@ void ControlPanel::Update()
 
 void ControlPanel::Draw(int x, int y, int num)
 {
-	//Novice::DrawSprite(x, y, GH_[num], 40, 40, 0, color_[num]);
-	Novice::DrawSpriteRect(x, y, GHDrawSize_.x * num, 0, GHDrawSize_.x, GHDrawSize_.y, GH_, (float)GHDrawSize_.x / (float)GHSize_.x, (float)GHDrawSize_.y / (float)GHSize_.y, 0, WHITE);
+	Novice::DrawSpriteRect(x, y, GHDrawSize_.x * num, 0, GHDrawSize_.x, GHDrawSize_.y, GH_, (float)GHDrawSize_.x / (float)GHSize_.x, (float)GHDrawSize_.y / (float)GHSize_.y, 0, isInPiece_[num] ? RED : WHITE);
 
 	for (int i = 0; i < 4; i++)
 		Novice::ScreenPrintf(200, 20 + i * 20, "%s", isInPiece_[i] ? "true" : "false");
